3d86607f7961908f4d70ebefbea41c91c17d0bdd
RosenRotbig1811 <RosenRotbig1811@users.noreply.github.com>
2016-11-09 04:37:33 +0300

    Add files via upload

---
+++ Windows_Close/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Windows_Close
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Выключение";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radio_Button1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Перезагрузка";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radio_Button2);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите время в мин";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 58);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Гибернация";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radio_Button3);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Отменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 141);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Планировщик";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Windows_Close/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Close
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_tick);
+            timer1.Interval = 100;
+            timer1.Start();
+        }
+            public void timer1_tick(object sender, EventArgs e)
+            {
+                DateTime time = DateTime.Now;
+                string time_string;
+                time_string = time.ToString();
+                label1.Text = time_string;
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                if (radioButton1.Checked)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        string shut_time;
+                        shut_time = (Convert.ToInt32(textBox1.Text) * 60).ToString();
+                        System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                        exit.StartInfo.FileName = "cmd.exe";
+                        exit.StartInfo.Arguments = "/c shutdown -s -t  " + shut_time;
+                        exit.Start();
+                    }
+                    else
+                    {
+                        System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                        exit.StartInfo.FileName = "cmd.exe";
+                        exit.StartInfo.Arguments = "/c shutdown -s";
+                        exit.Start();
+                    }
+                }
+
+                if (radioButton2.Checked)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        string shut_time;
+                        shut_time = (Convert.ToInt32(textBox1.Text) * 60).ToString();
+                        System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                        exit.StartInfo.FileName = "cmd.exe";
+                        exit.StartInfo.Arguments = "/c shutdown -r -t " + shut_time;
+                        exit.Start();
+                    }
+                    else
+                    {
+                        System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                        exit.StartInfo.FileName = "cmd.exe";
+                        exit.StartInfo.Arguments = "/c shutdown -r";
+                        exit.Start();
+                    }
+                }
+                if(radioButton3.Checked)
+                {
+                    this.textBox1.Enabled = false;
+                        System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                        exit.StartInfo.FileName = "cmd.exe";
+                        exit.StartInfo.Arguments = "/c shutdown -h";
+                        exit.Start();
+                }
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                System.Diagnostics.Process exit = new System.Diagnostics.Process();
+                exit.StartInfo.FileName = "cmd.exe";
+                exit.StartInfo.Arguments = "/c shutdown -a";
+                exit.Start();
+            }
+
+            private void radio_Button3(object sender, EventArgs e)
+            {
+                textBox1.Enabled = false;
+            }
+
+            private void radio_Button2(object sender, EventArgs e)
+            {
+                textBox1.Enabled = true;
+            }
+
+            private void radio_Button1(object sender, EventArgs e)
+            {
+                textBox1.Enabled = true;
+            }
+            
+        }
+}
+
